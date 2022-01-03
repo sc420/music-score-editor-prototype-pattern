1a11421dd482b7c4e6828d395ad5c56ed075e5be
Shawn Chang <ebola777gm@gmail.com>
2022-01-04 01:46:33 +0800

    fix constraining rotated items

--- src/app/graphic.py
+++ src/app/graphic.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, List
+from typing import Any, cast, List
 import math
 
 from PySide2 import QtCore, QtSvg, QtWidgets
@@ -13,15 +13,18 @@ class Graphic(QtWidgets.QGraphicsItemGroup):
         self.init_flags()
         self.init_transform_origin_point()
 
-    def itemChange(self,
-                   change: QtWidgets.QGraphicsItem.GraphicsItemChange,
-                   value: Any):
+    def itemChange(
+        self, change: QtWidgets.QGraphicsItem.GraphicsItemChange, value: Any
+    ) -> Any:
         # Reference: https://www.walletfox.com/course/qgraphicsitemsnaptogrid.php
-        if (change == QtWidgets.QGraphicsItem.ItemPositionChange
-                and self.scene()):
-            value = self.constrain_item_inside_scene(value)
-            value = self.snap_item_to_nearest_item(value)
-            return value
+        if (
+            change == QtWidgets.QGraphicsItem.ItemPositionChange
+            and self.scene()
+        ):
+            new_pos = cast(QtCore.QPointF, value)
+            new_pos = self.constrain_item_inside_scene(new_pos)
+            new_pos = self.snap_item_to_nearest_item(new_pos)
+            return new_pos
 
         return super().itemChange(change, value)
 
@@ -38,51 +41,70 @@ class Graphic(QtWidgets.QGraphicsItemGroup):
     def init_transform_origin_point(self):
         self.setTransformOriginPoint(self.get_center_translation())
 
-    def constrain_item_inside_scene(self, value: Any) -> Any:
+    def constrain_item_inside_scene(
+        self, new_pos: QtCore.QPointF
+    ) -> QtCore.QPointF:
+        # Calculate new bounding rectangle if we apply the new position
         scene_rect = self.scene().sceneRect()
-        item_rect = self.boundingRect()
-        item_rect.moveTopLeft(value)
+        item_rect = self.sceneBoundingRect()
+        delta = new_pos - self.pos()
+        item_rect.translate(delta)
         if not scene_rect.contains(item_rect):
-            # Keep the item inside the scene rect
-            value.setX(min(scene_rect.right() - item_rect.width(),
-                       max(value.x(), scene_rect.left())))
-            value.setY(min(scene_rect.bottom() - item_rect.height(),
-                       max(value.y(), scene_rect.top())))
-            return value
-        return value
+            # Snap to the edges
+            if item_rect.left() < scene_rect.left():
+                adjustment_x = scene_rect.left() - item_rect.left()
+            elif scene_rect.right() < item_rect.right():
+                adjustment_x = scene_rect.right() - item_rect.right()
+            else:
+                adjustment_x = 0
 
-    def snap_item_to_nearest_item(self, value: Any) -> Any:
+            if item_rect.top() < scene_rect.top():
+                adjustment_y = scene_rect.top() - item_rect.top()
+            elif scene_rect.bottom() < item_rect.bottom():
+                adjustment_y = scene_rect.bottom() - item_rect.bottom()
+            else:
+                adjustment_y = 0
+
+            new_pos.setX(new_pos.x() + adjustment_x)
+            new_pos.setY(new_pos.y() + adjustment_y)
+        return new_pos
+
+    def snap_item_to_nearest_item(
+        self, new_pos: QtCore.QPointF
+    ) -> QtCore.QPointF:
         if not self.can_snap_to_others():
-            return value
+            return new_pos
 
-        src_snap_point = value + self.get_snap_point_translation()
+        src_snap_point = new_pos + self.get_snap_point_translation()
 
         scene_rect = self.scene().sceneRect()
         items = self.scene().items(scene_rect)
 
         # Get list of other groups
-        groups = filter(lambda obj: (
-            isinstance(obj, Graphic) and obj != self
-        ), items)
+        groups = filter(
+            lambda obj: (isinstance(obj, Graphic) and obj != self), items
+        )
 
         # Keep only the groups that can be snapped
         groups = filter(lambda obj: obj.can_be_snapped(), groups)
 
         # Keep only the groups that have bounding rectangles on the source snap
         # point (e.g., the hole of the whole note)
-        groups = filter(lambda obj: (
-            obj.sceneBoundingRect().contains(src_snap_point)
-        ), groups)
+        groups = filter(
+            lambda obj: (obj.sceneBoundingRect().contains(src_snap_point)),
+            groups,
+        )
 
         nearest_snap_point = None
-        nearest_snap_distance = float('inf')
+        nearest_snap_distance = float("inf")
         for group in groups:
             local_src_snap_point = group.mapFromScene(src_snap_point)
             local_dest_snap_point = group.get_dest_snap_point(
-                local_src_snap_point)
+                local_src_snap_point
+            )
             dest_snap_point = group.mapToScene(local_dest_snap_point)
 
-            delta = dest_snap_point - value
+            delta = dest_snap_point - new_pos
             snap_distance = math.sqrt((delta.x() ** 2) + (delta.y() ** 2))
             if snap_distance < nearest_snap_distance:
                 nearest_snap_point = dest_snap_point
@@ -91,9 +113,8 @@ class Graphic(QtWidgets.QGraphicsItemGroup):
         if nearest_snap_point:
             # We need to minus the translation since we want the point of the
             # top-left coordinate, not the snap point
-            value = (nearest_snap_point -
-                     self.get_snap_point_translation())
-        return value
+            new_pos = nearest_snap_point - self.get_snap_point_translation()
+        return new_pos
 
     def clone(self) -> Graphic:
         raise NotImplementedError()
@@ -171,7 +192,8 @@ class Staff(Graphic):
 
     def get_dest_snap_point(self, local_pos: QtCore.QPointF) -> QtCore.QPointF:
         lines_size = QtCore.QSizeF(
-            self.horizontal_distance, self.num_lines * self.vertical_gap)
+            self.horizontal_distance, self.num_lines * self.vertical_gap
+        )
         snap_area = QtCore.QRectF(self.lines_offset, lines_size)
 
         # Slightly expand the snap area
@@ -186,7 +208,7 @@ class Staff(Graphic):
             return local_pos
 
         snap_y = None
-        closest_diff = float('inf')
+        closest_diff = float("inf")
         # Line notes
         for i in range(self.num_lines):
             y = self.lines_offset.y() + i * self.vertical_gap
@@ -218,9 +240,7 @@ class WholeNote(MusicalNote):
         return cloned_graphic
 
     def create_children(self) -> List[QtWidgets.QGraphicsItem]:
-        items = [
-            QtSvg.QGraphicsSvgItem(":/graphics_view/icons/whole_note.svg")
-        ]
+        items = [QtSvg.QGraphicsSvgItem(":/graphics_view/icons/whole_note.svg")]
         return items
 
     def get_snap_point_translation(self) -> QtCore.QPointF:
@@ -235,9 +255,7 @@ class HalfNote(MusicalNote):
         return cloned_graphic
 
     def create_children(self) -> List[QtWidgets.QGraphicsItem]:
-        items = [
-            QtSvg.QGraphicsSvgItem(":/graphics_view/icons/half_note.svg")
-        ]
+        items = [QtSvg.QGraphicsSvgItem(":/graphics_view/icons/half_note.svg")]
         return items
 
     def get_snap_point_translation(self) -> QtCore.QPointF:
